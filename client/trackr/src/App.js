d0abbf5455b187c5df46f1bac581c06ac851e027
Parker Rudd <parkerbrudd@gmail.com>
2022-06-27 22:18:00 -0600

    designed homepage

--- client/trackr/src/App.js
+++ client/trackr/src/App.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ReactDOM } from 'react'
+import {BrowserRouter as Router, Routes, Route, useNavigate} from 'react-router-dom'
+import {Navigate} from 'react-router-dom'
+import styles from './styles/style.css'
 
 function App() {
+  const [goToExercises, setGoToExercises] = useState(false)
+
+  if (goToExercises) {
+    return <Navigate to="/Exercises"/>
+  }
+
   return (
     <div className="App">
-      <h1>trackr</h1>
+      <div className="homepage-container">
+        <div className="left-screen">
+          <h1>trackr</h1>
+          <h3>Organize your workouts to maximize your progress.</h3>
+        </div>
+        <div className="right-screen">
+          <div className="add-workout" onClick={() => setGoToExercises(true)}>Add Exercises</div>
+          <div className="calender">Check Calender</div>
+          <div className="start-workout">Start Workout</div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ client/trackr/src/exercises.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Exercises() {
+
+    return (
+        <div>Workouts</div>
+    )
+}
+
+export default Exercises; 
